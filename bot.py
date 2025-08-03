ac01df3da8c1ad099cbb8490765f17a1f0d3f58b
courseley-cmd <courseley@gmail.com>
2025-08-04 00:50:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,18 +67,15 @@ async def handle_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
         await update.message.reply_text(f"✅ Заявка принята. Стол: {table}")
         context.user_data["status"] = None
 
-import asyncio
-
-async def main():
+def main():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_text))
     app.add_handler(MessageHandler(filters.ALL, handle_webhook))
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-    await app.updater.idle()
+
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
